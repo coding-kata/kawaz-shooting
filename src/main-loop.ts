e9afa5a24245d20478f0286ff8b7b8d1ff3b2782
azu <azuciao@gmail.com>
2015-04-20 00:08:48 +0900

    refactor(lib): main loop use requestAnimationFrame
    - calc moving px using delta value

--- src/main-loop.ts
+++ src/main-loop.ts
@@ -1,23 +1,20 @@
 "use strict";
 // メインループ - 60 FPS のゲームの場合約 17 ms
 // 実行環境によってゲーム速度が変化するので、それを整えるためのFPS
-var FPS = 30;
-var MSPF = 1000 / FPS;
+export var FPS = 60;
+var lastTimeStamp:number;
+var render:Function;
+// http://qiita.com/zukkun/items/43466ba12e94b90a9f8d
+function tick(timeStamp:number) {
+    // 前回の呼び出しからの経過時間
+    var deltaTime = (timeStamp - lastTimeStamp) / 1000;
+    // var currentFPS = 1000 / (timeStamp - lastTimeStamp);
+    lastTimeStamp = timeStamp;
+    render(deltaTime);
+    requestAnimationFrame(tick);
+}
 export default function mainLoop(enterFrame:Function) {
-    var startTime = Date.now();
-    //処理
-    enterFrame();
-    // 処理の経過時間
-    var deltaTime:number = Date.now() - startTime;
-    // 次のループまでのintervalを算出
-    var interval = MSPF - deltaTime;
-    if (interval > 0) {
-        // interval分を待つ
-        setTimeout(()=>{
-            mainLoop(enterFrame);
-        }, interval);
-    } else {
-        // 次の処理へ
-        mainLoop(enterFrame);
-    }
+    lastTimeStamp = performance.now();
+    render = enterFrame;
+    requestAnimationFrame(tick);
 }
